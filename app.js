dc7fdd8ef2e91913a1dcff3479147c33b63e68f9
gonzalolsk <gonzalolsk@gmail.com>
2021-05-25 09:44:23 -0300

    correccion de los get en app.js y prueba de links

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.listen(3000, () => console.log('Servidor corriendo'));
 // });
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'index.html')))
-app.get('/login',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'carrito_de_compra.html')));
-app.get('/login',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'detalle_producto.html')));
+app.get('/carrito_de_compra',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'carrito_de_compra.html')));
+app.get('/detalle_producto',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'detalle_producto.html')));
 app.get('/login',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'login.html')));
-app.get('/register',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'registro.html')));
+app.get('/registro',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'registro.html')));

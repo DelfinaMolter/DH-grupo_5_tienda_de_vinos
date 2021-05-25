3a359b7d6191878e33d05072925ff08167fc97fa
gonzalolsk <gonzalolsk@gmail.com>
2021-05-25 09:54:31 -0300

    edite app.js para hacer el app.use(express.static... a la carpeta public

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const path = require ('path');
 const app = express(); 
 app.listen(3000, () => console.log('Servidor corriendo'));
 
-// app.get ('/',(req,res)=>{
-//     res.send('Bienvenido al sitio');
-// });
+app.use(express.static(path.resolve(__dirname,'public')))
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'index.html')))
 app.get('/carrito_de_compra',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'carrito_de_compra.html')));

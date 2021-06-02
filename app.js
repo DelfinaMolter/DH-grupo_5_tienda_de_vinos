796efda741c6f2508e0f709ecf3e8842941b99ee
gonzalolsk <gonzalolsk@gmail.com>
2021-06-02 17:16:48 -0300

    modifique nombre de archivos con guiones bajos

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.listen(app.get("port"), () => console.log('Servidor esta corriendo en http:/
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')))
-app.get('/carrito_de_compra',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'carrito_de_compra.html')));
-app.get('/detalle_producto',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'detalle_producto.html')));
+app.get('/carrito-de-compra',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'carrito-de-compra.html')));
+app.get('/detalle',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'detalle.html')));
 app.get('/login',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get('/registro',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'registro.html')));
 

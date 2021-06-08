35cf3cef816a6a97b70f5a1ca71a667e9a259ccf
DelfinaMolter <delfi.molter@gmail.com>
2021-06-08 12:27:51 -0300

    modifico los links de registro en los html de detalle y login, y modifique el nombre de carrito-de-compra por carrito.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.listen(app.get("port"), () => console.log('Servidor esta corriendo en http:/
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')))
-app.get('/carrito-de-compra',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'carrito-de-compra.html')));
+app.get('/carrito',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'carrito.html')));
 app.get('/detalle',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'detalle.html')));
 app.get('/login',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get('/registro',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'registro.html')));

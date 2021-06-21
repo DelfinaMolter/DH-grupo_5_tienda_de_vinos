88d324de43427ea1eb717a0b8365137897d99efd
gonzalolsk <gonzalolsk@gmail.com>
2021-06-21 19:35:03 -0300

    css de create y edit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const controller = {
     login: (req,res) => {res.render('./users/login')},
-    register: (req,res)=>{res.render('./users/registro')}
+    register: (req,res)=>{res.render('./users/registro')},
+    edit: (req,res)=>{res.render('./products/edit')}
 }
 module.exports = controller;

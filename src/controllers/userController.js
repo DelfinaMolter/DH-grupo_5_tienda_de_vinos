ae69d599fcf19d495b7b6f9f8edae8be983c8124
gonzalolsk <gonzalolsk@gmail.com>
2021-06-22 20:23:13 -0300

    modificaciones de la correccion del sprint 3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 const controller = {
-    login: (req,res) => {res.render('./users/login')},
-    register: (req,res)=>{res.render('./users/registro')},
-    edit: (req,res)=>{res.render('./products/edit')}
+    login: (req,res) => {res.render('users/login')},
+    register: (req,res)=>{res.render('users/registro')},
+    edit: (req,res)=>{res.render('products/edit')}
 }
 module.exports = controller;

0a841686b0c7a107fc78d8d607cad3de7ec78a70
gonzalolsk <gonzalolsk@gmail.com>
2021-07-10 10:59:16 -0300

    modificaciones de la correccion

--- src/middlewares/multerMiddlewares.js
+++ src/middlewares/multerMiddlewares.js
@@ -1,6 +1,5 @@
 const multer = require('multer');
 const path = require('path');
-const { nextTick } = require('process');
 const storage = multer.diskStorage({
     destination:(req, file, cb) => {
         cb(null, './public/img/products/wines');

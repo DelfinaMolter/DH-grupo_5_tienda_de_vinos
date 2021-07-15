ba7128f16a1bf852bdc8acb818db2ae6405f1f01
DelfinaMolter <delfi.molter@gmail.com>
2021-07-15 15:35:49 -0300

    se agregaron los ejs de users y profile y se vincularon con los routes y los controladores

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
 const controller = {
     login: (req,res) => {res.render('users/login')},
     register: (req,res)=>{res.render('users/registro')},
-    edit: (req,res)=>{res.render('products/edit')}
+    edit: (req,res)=>{res.render('products/edit')},
+    users: (req,res)=>{res.render('users/users')},
+    profile: (req,res)=>{res.render('users/profile')}
 }
 module.exports = controller;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,7 @@ const user = require('../controllers/userController')
 
 router.get('/login', user.login)
 router.get('/registro', user.register)
+router.get('/users', user.users)
+router.get('/profile', user.profile)
 
 module.exports = router

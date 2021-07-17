f1a072d035bcfa66ff7a50f9ef2ed3e098014158
gonzalolsk <gonzalolsk@gmail.com>
2021-07-17 17:26:11 -0300

    merge

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const uploadFile = require('../middlewares/multerMiddlewares');
 const user = require('../controllers/userController')
 
 router.get('/login', user.login)
 router.get('/registro', user.register)
+
+router.post('/registro',  [uploadFile.single('avatar'), ACAVANLASVALIDACIONES], user.processRegister);
 
 module.exports = router

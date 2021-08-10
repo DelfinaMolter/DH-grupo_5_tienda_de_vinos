baaae37d13fab4531f9b6c9a163df07072cbc1c2
gonzalolsk <gonzalolsk@gmail.com>
2021-08-10 12:22:05 -0300

    checkeo de mvc users

--- src/middlewares/multerMiddlewares.js
+++ src/middlewares/multerMiddlewares.js
@@ -4,7 +4,7 @@ const path = require('path');
 const func = (folder) => {
     return  multer.diskStorage({
         destination:(req, file, cb) => {
-            cb(null, './public/img/products/' + folder);
+            cb(null, './public/img/' + folder);
         },
         filename:(req, file, cb) => {
             let fileName = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,9 +5,7 @@ const multer = require('multer');
 
 //Middlewares
 const validations = require('../middlewares/validationRegisterMiddlewares');
-//const login = require('../middlewares/userLoginValidations');
 const controller = require('../controllers/mainController');
-const admin = require('../middlewares/adminMiddlewares');
 const guestMiddlewares = require('../middlewares/guestMiddlewares');
 const uploadFile = require('../middlewares/multerMiddlewares')
 
@@ -28,14 +26,6 @@ router.post('/registro',[upload.single('img'), validations], userController.proc
 //Logout
 //router.get('/logout/', userController.logout);
 
-//router.post('/login', validations, userController.users);
-//router.post("/access",[login,admin], controller.access) 
-//validations, userController.processRegister);
 
-//------------------ROUTER LOGIN-------------------
-/*router.post('/login', validations/*,  [
-    check('Usuari@').isUser().whitMessage('Usuario inválido'),
-    check('Contraseña').isLength({min: 6}).whitMessage('La contraseña debe contener al menos 6 caractéres')
-] ,userController.processLogin);*/
 
 module.exports = router

92fff6c94a455de9ecec2b54592cb5dc4795454c
gonzalolsk <gonzalolsk@gmail.com>
2021-08-18 09:38:15 -0300

    productsControllerDB

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController');
+// const productController = require('../controllers/productController');
 
 //Middlewares
 const multer = require('multer');
@@ -9,15 +9,18 @@ const uploadFile = require('../middlewares/multerMiddlewares')
 const upload = multer({storage:uploadFile('products')});
 
 
-router.get('/', productController.products);
-router.get('/detalle/:id', productController.detalle);
-router.get('/crear', productController.create);
-router.get('/editar/:id', productController.edit);
+// router.get('/', productController.products);
+// router.get('/detalle/:id', productController.detalle);
+// router.get('/crear', productController.create);
+// router.get('/editar/:id', productController.edit);
 
 
-router.post('/crear', upload.single('product-img'), productController.save)
-router.delete('/delete/:id', productController.delete)
-router.put("/edit/:id", productController.update)
+// router.post('/crear', upload.single('product-img'), productController.save)
+// router.delete('/delete/:id', productController.delete)
+// router.put("/edit/:id", productController.update)
 
+const productControllerDB = require('../controllers/productControllerDB');
+
+router.get('/', productControllerDB.list);
 
 module.exports = router

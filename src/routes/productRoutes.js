fdae5b42c650d8443599b427c0d5cf8f28d93fa8
emendiolaza <emanuelmendiolaza@gmail.com>
2021-09-07 09:29:23 -0300

    validacion back productos

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -40,11 +40,8 @@ router.post('/crear',[
 
 //Actualizacion de productos
 router.get('/editar/:id' ,productControllerDB.edit);
-router.put('/edit/:id', upload.single('img') ,productControllerDB.update);
-//-REVISAR RUTA  SI ESTA OK
-router.post('/edit/id',[
-    upload.single('img'),validations],
-    productControllerDB.store);
+router.put('/editar/:id', upload.single('img') ,productControllerDB.update);
+
 
 
 

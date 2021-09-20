a8570d109988913dcd9192f272dc1843db2ce09d
emendiolaza <emanuelmendiolaza@gmail.com>
2021-09-20 12:38:56 -0300

    crecion de Api de usuarios

--- src/controllers/api/usersApiController.js
+++ src/controllers/api/usersApiController.js
@@ -1,52 +1,73 @@
 /*const db = require ('../../database/models');
-
-
-usersApis: async (req, res) => {
-
-    try{
-        let users = await db.users.findAll({
-            atributes: [
-                'id',
-                'name',
-                'email',
-                [fn('concat', 'http://localhost:3000/api/')]
-            ]
-        })
-
-        let = await db.User.findAll({
-            include:[{
-                association: 'users',
-                atributes: [[fn('count', col('name')), 'counts']]
-            }]
-        })
-    }
-}
-
-
-/*
+const express = require ('express');
+const app = express(); 
+app.set("port", process.env.PORT || 3000);
 
 module.exports = {
-    list: (req, res) => {
-        DB.tienda_de_vinos_db
-        .findAll()
-        .then(users => {
-            return res.json ({
-                total: users.length,
-                data: users,
-                status: 10
-            })
-        })
-    }
-}
-
-module.exports = {
-    show: (req, res) => {
+        list: (req,res) => {
         db.User
+        .findAll( {include: ['wineries', 'style_wines', 'grapes', ]})
+        .then (async users => {
+            let allUsers= await users.map(e=>{
+                return {id:e.id,
+                    first_name:e.first_name,
+                    last_name:e.last_name,
+                    email:e.email,
+                    img:e.image,
+                    birthDate:e.birthDate,
+                    dni:e.dni,
+                    url: `http://localhost:${app.get("port")}/api/detail/${e.id}`
+                }})
+            let find_style_winesTinto= allUsers.filter(e=>{
+                return e.style_wines== 'Vino Tinto'
+            })
+            let find_style_winesBlanco= allUsers.filter(e=>{
+                return e.style_wines== 'Vino Blanco'
+            })
+            let find_style_winesEspumante= allUsers.filter(e=>{
+                return e.style_wines== 'Espumantes'
+            })
+            // let grape= await db.Grape.findAll()
+            // let coundAndFind = (lista, valor)=> allProducts.filter(e=>{
+            //     return e.lista== valor
+            // })
+            return res.status(200).json( await
+                {   
+                    status:200,
+                    contador: {
+                        productosTotales:allUsers.length,
+                        contadorDeStyle_wines:{
+                            VinoTinto: find_style_winesTinto.length,
+                            VinoBlanco: find_style_winesBlanco.length,
+                            VinoEspumante: find_style_winesEspumante.length
+                        },
+                        contadorWineries:[],
+                        contadorGrapes:{
+                            SauvignonBlanc:[],
+                            Malbec:[]
+                        }
+                    },
+                    detalleProductos: allUsers
+                }
+                )
+            })
+    },
+    show: (req, res) => {
+        db.Product
         .findByPk(req.params.id)
-        .then(users => {
+        .then (async product => {
             return res.status(200).json({
-                
+                id:product.id,
+                name:product.name,
+                description:product.description,
+                img:product.image,
+                stock:product.stock,
+                in_sale:product.in_sale,
+                wineriesId:product.wineries_id,
+                wineries: await product.getWineries(), 
+                status: 200,
             })
         })
     }
-}*/
+} */
+
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 
 // Middlewares
 const validations = require('../middlewares/validationRegisterMiddlewares');
+const controller = require('../controllers/mainController');
 const guestMiddlewares = require('../middlewares/guestMiddlewares');
 const uploadFile = require('../middlewares/multerMiddlewares')
 
@@ -22,7 +23,7 @@ router.get('/detalle/:id', userControllerDB.detail);
 // router.get('/registro', guestMiddlewares, userController.register)
 router.get("/registro", guestMiddlewares, userControllerDB.register);
 // router.post('/registro',[upload.single('img'), validations], userController.processRegister);
-router.post("/registro",[upload.single('img'), validations], userControllerDB.processRegister);
+router.post("/registro",[validations, upload.single('img')], userControllerDB.processRegister);
 
 // router.get('/login', guestMiddlewares, userController.login)
 router.get('/login', guestMiddlewares, userControllerDB.login)
@@ -33,6 +34,8 @@ router.get('/logout', userControllerDB.logoutProcess);
 
 router.get('/edit/:id', userControllerDB.edit);
 router.put('/update/:id', userControllerDB.update);
+
+//Usuario API
 
 
 
